e568e13d8b92bf41110ac8b7d142681903d28bf5
Mag-Gonzalez <miguelgonza11425@gmail.com>
2026-09-01 15:06:36 -0500

    caqmbio en el server.js

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.render('index', { mensaje: '¡Hola Mundo desde EJS y Node.js!' });
 });
 
+// NUEVA RUTA DE ENSAYO: Módulo de prueba para Miguel y Bryan
+app.get('/prueba', (req, res) => {
+  res.send('<h1>Ruta de prueba funcionando al 100% - Equipo listo para el éxito</h1>');
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor activo en http://localhost:${PORT}`);
 });
